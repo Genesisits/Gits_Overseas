5b23364b03ad21ec3bf1c3c66637a298d89e6eae
jayanth <karupartijayanth143@gmail.com>
2023-03-31 15:35:07 +0530

    forgot password css

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from flask_mail import *
 from flask_mysqldb import MySQL
 from random import *
 
+
 app = Flask(__name__)
+
+# Connect to the database
+
 app.secret_key = "abc123"
 
 app = Flask(__name__)
@@ -12,7 +16,7 @@ app.secret_key = "abc123"
 app.secret_key="keyvalue"
 app.config["MYSQL_HOST"]="localhost"
 app.config["MYSQL_USER"]="root"
-app.config["MYSQL_PORT"] = 3306
+app.config["MYSQL_PORT"] = 3308
 app.config["MYSQL_PASSWORD"]=""
 app.config["MYSQL_DB"]="project"
 app.config["MYSQL_CURSORCLASS"]="DictCursor"
@@ -20,9 +24,9 @@ app.config["MYSQL_CURSORCLASS"]="DictCursor"
 mysql = MySQL(app)
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_USERNAME"] = "saicharansuraram@gmail.com"
+app.config["MAIL_USERNAME"] = "jayanthkaruparti.CCBPian00101@gmail.com"
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_PASSWORD"] = "jvvdkefwrvzdqcnz"
+app.config["MAIL_PASSWORD"] = "wdhobdfeamlgwthh"
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USE_TLS"] = False
 mail = Mail(app)
@@ -128,16 +132,71 @@ def student():
 def studentstatus():
     return render_template("studentstatus.html")
 
+@app.route('/forgotpasswordpage',methods=['GET','POST'])
+def forgotpasswordpage():
+    if request.method == 'POST':
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        c = cur.execute('select email from usertable where email = %s',(email,))
+        mysql.connection.cursor()
+        if c>0:
+            result = cur.fetchone()
+            print(result)
+            email = result['email']
+            session.permanent = True
+            session['email'] = email
+            flash("check for OTP")
+            msg = Message('subject', sender="jayanthkaruparti.CCBPian00101@gmail.com", recipients=[email])
+            msg.body = "THIS IS YOUR OTP" + str(otp)
+            mail.send(msg)
+            return redirect(url_for('fpsend'))
+        else:
+            error = "oops something went wrong"
+            return render_template("forgotpasswordpage.html",error = 'error')
+        cur.close()
+    return render_template("forgotpasswordpage.html")
+@app.route("/fpsend")
+def fpsend():
+    return render_template("fpsend.html")
+
+@app.route('/fpvalidate',methods=['POST'])
+def fpvalidate():
+    user_otp = request.form['otp']
+    if otp == int(user_otp):
+        return redirect(url_for("updatepassword"))
+    else:
+        error = "wrong OTP"
+        return render_template("fpsend.html", error=error)
+
+@app.route('/updatepassword',methods=['GET','POST'])
+def updatepassword():
+    if request.method == 'POST':
+        newpassword = request.form['newpassword']
+        confirmpassword = request.form['confirmpassword']
+        email = session['email']
+        if newpassword == confirmpassword:
+            cur = mysql.connection.cursor()
+            a = cur.execute('UPDATE usertable SET password = %s WHERE email = %s', [newpassword, email])
+            mysql.connection.commit()
+
+            if a > 0:
+                flash("password updated successfully")
+                return redirect(url_for("login"))
+            else:
+                error = "oops something went wrong"
+                return render_template("updatepassword.html",error = error)
+            cur.close()
+    return render_template("updatepassword.html")
+
+
 @app.route('/register',methods = ['GET','POST'])
 def register():
     if request.method == 'POST':
-
         fullname = request.form['fullname']
-
         fathername = request.form['fathername']
         contact = request.form['contact']
         email = request.form['email']
-        msg = Message('subject', sender="saicharansuraram@gmail.com", recipients=[email])
+        msg = Message('subject', sender="jayanthkaruparti.CCBPian00101@gmail.com", recipients=[email])
         msg.body = "THIS IS YOUR OTP" + str(otp)
         mail.send(msg)
         password = request.form['password']
@@ -150,7 +209,7 @@ def register():
         maritial = request.form['maritial']
         reference = request.form['reference']
         cur = mysql.connection.cursor()
-        br = cur.execute('insert into usertable(fullname,fathername,contact,email,password,confirmpassword,passport,country,qualification,location,gender,maritial,reference) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',(fullname,fathername,contact,email,password,confirmpassword,passport,country,qualification,location,gender,maritial,reference))
+        br = cur.execute('insert into usertable(fullname,fathername,contact,email,password,passport,country,qualification,location,gender,maritial,reference) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',(fullname,fathername,contact,email,password,passport,country,qualification,location,gender,maritial,reference))
         mysql.connection.commit()
         if br > 0 :
             if password == confirmpassword:
@@ -164,7 +223,6 @@ def register():
             return render_template("overseas.html",error = error)
         cur.close()
     return render_template("register.html")
-
 
 @app.route("/admin_register",methods=['GET','POST'])
 def admin_register():
@@ -318,7 +376,7 @@ def validate():
     else:
         error = "wrong OTP"
         return render_template("send.html", error=error)
-    cur
+
 
 @app.route("/send")
 def send():
@@ -370,7 +428,7 @@ def intake():
 def delete(email):
     cur = mysql.connection.cursor()
     print(email)
-    r = cur.execute("delete from usertable1 where email = %s", (email,))
+    r = cur.execute("delete from usertable where email = %s", (email,))
     mysql.connection.commit()
     print(r)
     if r>0:
